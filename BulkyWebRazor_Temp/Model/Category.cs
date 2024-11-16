4fe0d5aa64953c4eac4001588c0e65715f045af5
Andrzej Siebieranski <AndrzejS21@interia.pl>
2024-11-16 17:27:23 +0100

    End of section 2

---
+++ BulkyWebRazor_Temp/Model/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 100, ErrorMessage = "Display Order powinien wynosić między 1-100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
