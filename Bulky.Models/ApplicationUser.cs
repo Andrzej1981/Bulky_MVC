8871bffa32a8f92f8104458a08acdcafc01a5bd3
Andrzej Siebieranski <AndrzejS21@interia.pl>
2024-11-24 21:54:58 +0100

    End of section 9

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -18,8 +18,9 @@ namespace Bulky.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
-        //public string? PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
 
+        public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
         public Company Company { get; set; }
